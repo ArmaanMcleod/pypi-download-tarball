fb471953128fe470d4b31f6853d553b8f1e5e42a
OpticGenius <opticgenius@hotmail.com>
2018-11-22 02:22:37 +1100

    Renamed project

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# Python Tarball Downloader
+# Tarball Downloader
 
-[![PyPI version](https://badge.fury.io/py/python-tarball-downloader.svg)](https://badge.fury.io/py/python-tarball-downloader)
+[![PyPI version](https://badge.fury.io/py/tarball-downloader.svg)](https://badge.fury.io/py/python-tarball-downloader)
 
 Automates manual Python library installation through the *PyPi* `tar.gz` tarball. If no tarball is found, the program will look for next available `.zip` file and install the package. I've used this in the past when *pip* hasn't worked out, and I have to revert to manual installation of Python libraries. 
 
 #### Install with:
 
-`pip install python-tarball-downloader`
+`pip install tarball-downloader`
 
 ## Basic Usage
 
--- setup.py
+++ setup.py
@@ -1,9 +1,3 @@
-"""A setuptools based setup module.
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
-
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 from os import path
@@ -35,18 +29,18 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name="python-tarball-downloader",  # Required
+    name="tarball-downloader",  # Required
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
     #
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="1.2.2",  # Required
+    version="1.2.3",  # Required
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description="Automates manual installation of Python modules",  # Optional
+    description="Automates manual installation of Python modules.",  # Optional
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
     #
@@ -71,7 +65,7 @@ setup(
     #
     # This field corresponds to the "Home-Page" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url="https://github.com/OpticGenius/python-tarball-downloader",  # Optional
+    url="https://github.com/OpticGenius/tarball-downloader",  # Optional
     # This should be your name or the name of the organization which owns the
     # project.
     author="Armaan McLeod",  # Optional
@@ -159,7 +153,7 @@ setup(
     # maintainers, and where to support the project financially. The key is
     # what's used to render the link text on PyPI.
     project_urls={  # Optional
-        "Bug Reports": "https://github.com/OpticGenius/python-tarball-downloader/issues",
-        "Source": "https://github.com/OpticGenius/python-tarball-downloader",
+        "Bug Reports": "https://github.com/OpticGenius/tarball-downloader/issues",
+        "Source": "https://github.com/OpticGenius/tarball-downloader",
     },
 )
