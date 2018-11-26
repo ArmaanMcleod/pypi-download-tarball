b6586ae00705037668a1f4a13dbb547370792ef9
OpticGenius <opticgenius@hotmail.com>
2018-11-27 06:53:15 +1100

    Adding more tests

--- tests.py
+++ tests.py
@@ -11,6 +11,7 @@ from requests import get
 from os import remove
 from os import getcwd
 
+
 class TestDownloads(unittest.TestCase):
     def test_parse_file(self):
         with open("test.txt", mode="w") as f:
@@ -30,8 +31,7 @@ class TestDownloads(unittest.TestCase):
 
         remove(f.name)
 
-    def test_download_file(self):
-        link = "https://files.pythonhosted.org/packages/40/35/298c36d839547b50822985a2cf0611b3b978a5ab7a5af5562b8ebe3e1369/requests-2.20.1.tar.gz"
+    def download_package(self, link):
         filename = basename(link)
         response = get(link)
         path = join(getcwd(), filename)
@@ -39,12 +39,27 @@ class TestDownloads(unittest.TestCase):
         downloaded_file = download.run_download(
             filename=filename, response=response, path=path
         )
-        self.assertTrue(isfile(downloaded_file))
 
+        return downloaded_file
+
+    def test_download_file(self):
+        path = self.download_package(
+            link="https://files.pythonhosted.org/packages/40/35/298c36d839547b50822985a2cf0611b3b978a5ab7a5af5562b8ebe3e1369/requests-2.20.1.tar.gz"
+        )
+        self.assertTrue(isfile(path))
+        remove(path)
+
+        path = self.download_package(
+            link="https://files.pythonhosted.org/packages/2d/80/1809de155bad674b494248bcfca0e49eb4c5d8bee58f26fe7a0dd45029e2/numpy-1.15.4.zip"
+        )
+        self.assertTrue(isfile(path))
         remove(path)
 
     def test_process(self):
-        self.assertEqual(download.run_process(['ls']), 0)
+        self.assertEqual(download.run_process(["ls"]), 0)
+        self.assertEqual(download.run_process(["ls", "-la"]), 0)
+        self.assertRaises(FileNotFoundError, lambda: download.run_process(["blah"]))
+
 
 if __name__ == "__main__":
     unittest.main()
